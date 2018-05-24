37809b325801d605b700a936db2ecc51ef61f26a
Glitch (formdata-ms) <none>
2018-05-24 06:37:26 +0000

    🕴💖 Checkpoint ./server.js:1088472/802

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ var multer = require('multer'); //to require multer for uploading photos
 var bodyParser = require('body-parser');//to require body-parser to grab json form data
 var morgan=require('morgan');   
 
-var upload = multer.diskStorage({ dest: 'public/uploads/' }) //giving upload a destination directory
+var storage = multer.diskStorage({
+destination: function (req, file, cb) {
+ cb(null, './uploads/');
+    },
+ 
+}); //giving upload a destination directory
 
 var app = express();  //to use express
 app.use(bodyParser.json());
@@ -16,9 +21,11 @@ app.use(morgan('dev'));  //to use style.css
 app.set('view engine', 'ejs'); //to use ejs as template language
 // app.use(bodyParser.urlencoded({ extended: false })); //to use body parser
 
+app.get('/',(req,res)=>{
+  res.render('index');
+});
 
-
-app.post('/', upload.single('avatar'),(req,res)=>{
+app.post('/', multer({storage: storage, dest: './uploads/'}).single('uploads'),(req,res)=>{
   if(!req.file){
     console.log("No file recieved");
     return res.send({
